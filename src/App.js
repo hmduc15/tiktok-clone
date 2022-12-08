174b38eceddf3d8ede9b9bf2d10650c45ed074c6
hmduc15 <hmduc.m10@gmail.com>
2022-12-08 22:59:58 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Routes, Route, Link } from 'react-router-dom'
+
+import HomePage from './pages/Home'
+import ContactPage from './pages/Contact'
+import NewsPage from './pages/News'
+
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/News">News</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/News" element={<NewsPage />} />
+        <Route path="/Contact" element={<ContactPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
